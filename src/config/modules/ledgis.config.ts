a51e1d1fb13c29593effda07529ecf89f4479d58
Jang chorong <sweatpotato13@gmail.com>
2021-07-09 11:32:11 +0900

    fix: Fix Wrong config for LIT_CODE

--- src/config/modules/ledgis.config.ts
+++ src/config/modules/ledgis.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs("ledgis", () => ({
     nodeEndpoint: process.env.LEDGIS_LIT_ENDPOINT || "https://lit.ledgis.io",
-    code: process.env.LEDGIS_LIT_CODE || "lit",
+    code: process.env.LEDGIS_LIT_CODE || "led.lit",
 }));
