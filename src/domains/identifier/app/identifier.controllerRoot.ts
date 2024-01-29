a9c53d99f177970c1c6a70f9fb9fb453fe5709d5
xter <xter@ibct.kr>
2024-01-29 17:50:19 +0900

    add healthcheck response

--- src/domains/identifier/app/identifier.controllerRoot.ts
+++ src/domains/identifier/app/identifier.controllerRoot.ts
@@ -1,16 +1,8 @@
 import { Controller, Inject, Get, Header, Param } from "@nestjs/common";
-import { IDidDocument } from "@shared/interfaces/did";
-import { DidValidationPipe } from "@common/pipes/did-validation.pipe";
-import { IIdentifierService } from "@identifier/domain/interfaces/identifier.interface";
 
 @Controller()
 export class IdentifierControllerRoot {
-    constructor(
-        @Inject("IdentifierService")
-        private readonly _IdentifierService: IIdentifierService
-    ) {}
-
-    @Get("")
+    @Get()
     async handleRootRequest(): Promise<any> {
         // Add your logic here to handle requests to the root path
         return "HealthCheck.";
