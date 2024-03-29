2a39784db97b64f98ad2063e6c77e87c1d73b431
xter <xter@ibct.kr>
2024-03-29 11:25:36 +0900

    파라미터 없을때도 OK리턴

--- src/domains/identifier/app/identifier.controller.ts
+++ src/domains/identifier/app/identifier.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Inject, Get, Header, Param } from "@nestjs/common";
+import { Controller, Inject, Get, Header, Param, Res } from "@nestjs/common";
+import { Response } from "express";
 import { IDidDocument } from "@shared/interfaces/did";
 import { DidValidationPipe } from "@common/pipes/did-validation.pipe";
 import { IIdentifierService } from "@identifier/domain/interfaces/identifier.interface";
@@ -14,8 +15,13 @@ export class IdentifierController {
     @Get(":identifier")
     @Header("content-type", "application/did+ld+json")
     async findOneDidDocument(
-        @Param("identifier", DidValidationPipe) did: string
+        @Param("identifier", DidValidationPipe) did: string,
+        @Res() res: Response
     ): Promise<IDidDocument> {
+        if (!did) {
+            res.status(200).send(); // 파라미터가 없는 경우 OK 응답
+            return;
+        }
         return await this._IdentifierService.getDidDocument(did);
     }
 }
